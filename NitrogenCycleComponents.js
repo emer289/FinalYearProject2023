abd62708e595f83e4140d255831e94c2b9033a75
Emer Murphy <60451051+emer289@users.noreply.github.com>
2023-03-02 14:50:22 +0000

    removed code for when the NCC are in the water

--- NitrogenCycleComponents.js
+++ NitrogenCycleComponents.js
@@ -1,5 +1,3 @@
-
-
 class NitrogenCycleComponents {
 
     constructor(size, state, type, currentRegion, Region2, ifFer) {
@@ -20,9 +18,8 @@ class NitrogenCycleComponents {
         );
 
 
-            this.pos = new Coordinate(random(this.Region2.x + 50, this.bottomRight.x - 50),
-                random(this.Region2.y + this.Region2.height/2, this.bottomRight.y - 50))
-
+        this.pos = new Coordinate(random(this.Region2.x + 50, this.bottomRight.x - 50),
+            random(this.Region2.y + this.Region2.height / 2, this.bottomRight.y - 50))
 
 
         this.direction = new Coordinate(random(-0.75, 1.5), random(-0.75, 1.5));
@@ -31,63 +28,46 @@ class NitrogenCycleComponents {
         this.size = size;
         this.state = state;
 
-        if(type == "n2"){
+        if (type == "n2") {
             this.pic = n2Image;
-        }else if(type == "no2"){
+        } else if (type == "no2") {
             this.pic = no2Image
-        }else if(type == "nh4"){
+        } else if (type == "nh4") {
             this.pic = nh4Image
-        }else if(type == "no3"){
+        } else if (type == "no3") {
             this.pic = no3Image
-        }else if(type == "bacterium1"){
+        } else if (type == "bacterium1") {
             this.pic = bacteria1Image;
-        }else if(type == "bacterium2"){
+        } else if (type == "bacterium2") {
             this.pic = bacteria2Image;
-        }else{ //if(type == "bacterium3")
+        } else { //if(type == "bacterium3")
             this.pic = bacteria3Image;
         }
     }
 
 
-
     move() {
 
 
-
-        if(this.inWater && !this.inTransit){
-
-            if (this.pos.x + this.size >= water.x + water.width) {
-                this.direction.x *= -1;
-                //too far left
-            } else if (this.pos.x <= water.x) {
-                this.direction.x *= -1;
-                //to far down
-            } else if (this.pos.y + this.size >= water.y + water.height) {
-                this.direction.y *= -1;
-                //to far up
-            } else if (this.pos.y <= water.y ) {
-                this.direction.y *= -1;
-            }
-
-        }else if(this.inTransit && !this.inWater){
-            if(this.pos.y >= water.y && this.pos.x + this.size*2 > water.x + water.width ){
+        if (this.inTransit && !this.inWater) {
+            if (this.pos.y >= water.y && this.pos.x + this.size * 2 > water.x + water.width) {
                 this.direction.x -= .01;
                 this.direction.y = 0
-            }else if( this.pos.x <= water.x + water.width  && this.pos.y > water.y && this.pos.y + this.size < water.y + water.height) {
+            } else if (this.pos.x <= water.x + water.width && this.pos.y > water.y && this.pos.y + this.size < water.y + water.height) {
                 NitrogenCyclePop.splice(NitrogenCyclePop.indexOf(this), 1)
                 NitrogenCycleWaterPop.push(this)
                 waterQuality = waterQuality - 1
                 this.inWater = true;
                 this.inTransit = false
                 this.direction = new Coordinate(random(0.75, 1.5), random(0.75, 1.5));
-            }else{
+            } else {
 
                 //to far right
                 if (this.pos.x + this.size >= farm.x + farm.width) {
                     this.direction.x *= -1;
 
                     // to far left and
-                } else if (this.pos.x <= this.topLeft.x ) {
+                } else if (this.pos.x <= this.topLeft.x) {
                     this.direction.x *= -1;
 
                     //to far down
@@ -95,34 +75,34 @@ class NitrogenCycleComponents {
                     this.direction.y *= -1;
 
                     //to far up and
-                } else if (this.pos.y <= farm.y ) {
+                } else if (this.pos.y <= farm.y) {
                     this.direction.y *= -1;
                 }
 
             }
-        }else if (!this.inTransit && !this.inWater){
+        } else if (!this.inTransit && !this.inWater) {
 
-            if(isInside(VBS.x, VBS.y, VBS.x,VBS.topRighty, VBS.topRightx, VBS.topRighty, this.pos.x, this.pos.y)){
+            if (isInside(VBS.x, VBS.y, VBS.x, VBS.topRighty, VBS.topRightx, VBS.topRighty, this.pos.x, this.pos.y)) {
 
-                    let ind = NitrogenCyclePop.indexOf(this)
-                    //VBS grows
-                    if(this.type != "bacterium1" && this.type != "bacterium2" && this.type != "bacterium3"){
-                        NitrogenCyclePop.splice(ind, 1)
-                        for(let i=0; i<vbsToPlant.length; i++){
-                            vbsToPlant[i].size += 10
-                        }
-                    }else{
-                        this.direction.x *= -1;
-                        this.direction.y *= -1;
+                let ind = NitrogenCyclePop.indexOf(this)
+                //VBS grows
+                if (this.type != "bacterium1" && this.type != "bacterium2" && this.type != "bacterium3") {
+                    NitrogenCyclePop.splice(ind, 1)
+                    for (let i = 0; i < vbsToPlant.length; i++) {
+                        vbsToPlant[i].size += 10
                     }
+                } else {
+                    this.direction.x *= -1;
+                    this.direction.y *= -1;
+                }
 
-            }else{
+            } else {
                 //to far right
                 if (this.pos.x + this.size >= farm.x + farm.width) {
                     this.direction.x *= -1;
 
                     // to far left and
-                } else if (this.pos.x <= this.topLeft.x ) {
+                } else if (this.pos.x <= this.topLeft.x) {
                     this.direction.x *= -1;
 
                     //to far down
@@ -130,7 +110,7 @@ class NitrogenCycleComponents {
                     this.direction.y *= -1;
 
                     //to far up and
-                } else if (this.pos.y <= farm.y ) {
+                } else if (this.pos.y <= farm.y) {
                     this.direction.y *= -1;
                 }
             }
@@ -138,54 +118,54 @@ class NitrogenCycleComponents {
         }
 
 
-
         this.pos = new Coordinate(
-            this.pos.x + this.direction.x/4,
-            this.pos.y + this.direction.y/4
+            this.pos.x + this.direction.x / 4,
+            this.pos.y + this.direction.y / 4
         );
     }
 
 
-    checkCollision(nc2){
+    checkCollision(nc2) {
 
-            if(dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
-                && ((this.type == "n2" && nc2.type == "bacterium1"))
-            ){
+        if (dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
+            && ((this.type == "n2" && nc2.type == "bacterium1"))
+        ) {
 
 
-            }else if(dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
-                && (this.type == "bacterium1" && nc2.type == "n2")){
+        } else if (dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
+            && (this.type == "bacterium1" && nc2.type == "n2")) {
 
-                nc2.pic = nh4Image
-                nc2.type = "nh4"
+            nc2.pic = nh4Image
+            nc2.type = "nh4"
 
-            }else if(dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
-                && (this.type == "bacterium2" && nc2.type == "nh4")){
+        } else if (dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
+            && (this.type == "bacterium2" && nc2.type == "nh4")) {
 
-                nc2.pic = no2Image
-                nc2.type = "no2"
+            nc2.pic = no2Image
+            nc2.type = "no2"
 
-            }else if(dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
-                && (this.type == "nh4" && nc2.type == "bacterium2")){
+        } else if (dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
+            && (this.type == "nh4" && nc2.type == "bacterium2")) {
 
-                this.pic = no2Image
-                this.type = "no2"
+            this.pic = no2Image
+            this.type = "no2"
 
-            }else if(dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
-                && (this.type == "bacterium3" && nc2.type == "no2")){
+        } else if (dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
+            && (this.type == "bacterium3" && nc2.type == "no2")) {
 
-                nc2.pic = no3Image
-                nc2.type = "no3"
+            nc2.pic = no3Image
+            nc2.type = "no3"
 
-            }else if(dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
-                && (this.type == "no2" && nc2.type == "bacterium3")){
+        } else if (dist(this.pos.x, this.pos.y, nc2.pos.x, nc2.pos.y) < this.size
+            && (this.type == "no2" && nc2.type == "bacterium3")) {
 
-                this.pic = no3Image
-                this.type = "no3"
+            this.pic = no3Image
+            this.type = "no3"
 
-            }
+        }
 
     }
+
     render() {
         image(
             this.pic,
@@ -197,21 +177,19 @@ class NitrogenCycleComponents {
     }
 }
 
-function area(x1, y1, x2, y2, x3, y3)
-{
-    return Math.abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
+function area(x1, y1, x2, y2, x3, y3) {
+    return Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
 }
 
-function isInside(x1, y1, x2, y2, x3, y3, x, y)
-{
+function isInside(x1, y1, x2, y2, x3, y3, x, y) {
 
-    let A = area (x1, y1, x2, y2, x3, y3);
+    let A = area(x1, y1, x2, y2, x3, y3);
 
-    let A1 = area (x, y, x2, y2, x3, y3);
+    let A1 = area(x, y, x2, y2, x3, y3);
 
-    let A2 = area (x1, y1, x, y, x3, y3);
+    let A2 = area(x1, y1, x, y, x3, y3);
 
-    let A3 = area (x1, y1, x2, y2, x, y);
+    let A3 = area(x1, y1, x2, y2, x, y);
 
     return (A == A1 + A2 + A3);
 }
